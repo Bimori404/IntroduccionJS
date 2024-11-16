b8530972e9bedfe2bbeff338cf29e206f9678047
/Bimori <106877704+Bimori404@users.noreply.github.com>
2024-11-16 14:39:57 -0600

    dot

--- 15.-patrones.js
+++ 15.-patrones.js
@@ -1,26 +1,26 @@
-var filasCuadrado = 3;
+var filasCuadrado = 5;
 let asteriscos = '';
-for (let altura = 0; altura < filasCuadrado; altura++) { // Columnas
-    for (let relleno = 0; relleno < filasCuadrado; relleno++) { //filas
+for (let altura = 0; altura < filasCuadrado; altura++) {
+    for (let relleno = 0; relleno < filasCuadrado; relleno++) {
         asteriscos += '*';
     } asteriscos += '\n';
 } console.log(asteriscos);
 
 /*-------------------------------------------------------------------- */
 
-var filasPiramide = 3;
-for (let altura = 1; altura <= filasPiramide; altura++) { //controla la altura de la pirámide
+var filasPiramide = 5;
+for (let altura = 1; altura <= filasPiramide; altura++) {
     let asteriscos = '';
-    for (let agregar = 1; agregar <= altura; agregar++) { //se utiliza para agregar asteriscos a la fila actual
+    for (let agregar = 1; agregar <= altura; agregar++) {
         asteriscos += '*';
     }
     console.log(asteriscos);
 }
 
-console.log(" "); /*------------------------------------------------- */
+console.log(""); /*------------------------------------------------- */
 
 
-var filasRombo = 3;
+var filasRombo = 5;
 if (filasRombo % 2 === 0) {
     console.log("El número de filas debe ser impar.");
 } else {
@@ -30,11 +30,6 @@ if (filasRombo % 2 === 0) {
         for (let espBlanco = 0; espBlanco < Math.abs(mitad - altura); espBlanco++) {
             asteriscos += " ";
         }
-        /*
-            El número de asteriscos se calcula como 2 * (mitad - Math.abs(mitad - altura)) + 1.
-            La expresión 2 * (mitad - Math.abs(mitad - i)) calcula el número de asteriscos
-            en el centro de la fila, y luego se le suma 1 para incluir el asterisco central.
-        */
         for (let agregar = 0; agregar < 2 * (mitad - Math.abs(mitad - altura)) + 1; agregar++) {
             asteriscos += "*";
         }
@@ -42,5 +37,5 @@ if (filasRombo % 2 === 0) {
     }
 }
 
-console.log(" "); /*------------------------------------------------- */
+console.log(""); /*------------------------------------------------- */
 
